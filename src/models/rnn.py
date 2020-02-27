35423168fdad7c70dd4586ef613ba70539842f75
jdecid <josep.de.cid@gmail.com>
2020-02-27 12:46:18 +0100

    First version of imitation learning

--- src/models/rnn.py
+++ src/models/rnn.py
@@ -1,14 +1,61 @@
+import torch
 import torch.nn as nn
 
-from src.models.cnn import PoVModel
+from torch.nn import functional as F
 
 
-class ImitationModel(nn.Module):
-    def __init__(self):
+class ImitationRNNModel(nn.Module):
+    HIDDEN_LSTM_UNITS = 100
+    OUTPUT_CNN_UNITS = 128
+
+    def __init__(self, out_features: int, num_continuous: int):
         super().__init__()
 
-        self.cnn = PoVModel()
-        self.lstm = nn.LSTM(input_size=10 + 2, hidden_size=200)
+        self.out_features = out_features
+        self.num_continuous = num_continuous
+
+        self.cnn = ImitationCNNModel(self.OUTPUT_CNN_UNITS)
+        self.lstm = nn.LSTM(input_size=self.OUTPUT_CNN_UNITS, hidden_size=self.HIDDEN_LSTM_UNITS)
+        self.fc = nn.Linear(in_features=self.HIDDEN_LSTM_UNITS, out_features=out_features)
+
+    def forward(self, pov):
+        seq_len = pov.size(0)
+
+        x = self.cnn(pov)
+        out, _ = self.lstm(x.view(seq_len, 1, self.OUTPUT_CNN_UNITS))
+
+        out = F.relu(out)
+        out = self.fc(out)
+
+        # Set binary outputs in range [0, 1]
+        out[:, :, self.num_continuous:] = torch.sigmoid(out[:, :, self.num_continuous:])
+
+        return out
+
+
+class ImitationCNNModel(nn.Module):
+    def __init__(self, output_units: int):
+        super().__init__()
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(21632, 500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=output_units)
+        )
 
     def forward(self, x):
         bs = x.size(0)
--- src/navigate.py
+++ src/navigate.py
@@ -1,20 +1,36 @@
-import minerl
 import os
 
+import minerl
 import torch
-import torch.nn as nn
 from dotenv import load_dotenv
+from torchvision.transforms import Compose
 
-from src.models.rnn import ImitationModel
+from src.models.rnn import ImitationRNNModel
+from src.utils import ToTensor, ImitationLoss
 
 load_dotenv()
 
 minerl.data.download(os.environ['DATASET_DIR'], experiment='MineRLNavigate-v0')
 data = minerl.data.make('MineRLNavigate-v0', data_dir=os.environ['DATASET_DIR'])
 
-model = ImitationModel()
+model = ImitationRNNModel(out_features=11, num_continuous=2)
 optimizer = torch.optim.Adam(model.parameters())
-criterion = nn.MSELoss()
+criterion = ImitationLoss(num_continuous=2)
 
-for current_state, action, reward, next_state, done in data.sarsd_iter(num_epochs=1, max_sequence_len=32):
-    print(current_state['pov'].shape)
+transformation = Compose([ToTensor()])  # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+EPOCHS = 1
+SEQ_LEN = 32
+
+for current_state, action, reward, next_state, done in data.sarsd_iter(num_epochs=EPOCHS, max_sequence_len=SEQ_LEN):
+    optimizer.zero_grad()
+
+    pov = current_state['pov']
+    pov = torch.stack(list(map(transformation, pov)))
+
+    prediction = model(pov)
+
+    loss = criterion(prediction.squeeze(), action)
+    loss.backward()
+
+    optimizer.step()
